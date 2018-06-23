f9d9f42aedb89c6c4ceece8316eb760209c4f34b
Wavedorm <hiiwave@gmail.com>
2018-06-24 06:29:51 +0800

    Update twtos to patch on 2018.6.14 (Transcendence)
    Issues discovered:
    1. Some UI elements not translated (due to lack of corresponding string on itos data)

--- mymain.py
+++ mymain.py
@@ -5,16 +5,14 @@ from pathlib import Path
 lang_en_path = Path('data/lang-en')
 lang_tw_path = Path('data/lang-tw')
 output_path = Path('output/')
+# output_path = Path(r'C:\TreeOfSaviorTW\release\languageData\')
 
 
 matcher = Matcher(lang_en_path, lang_tw_path)
-# matcher.export('tw', 'full', output_path / 'twtos-full')
-matcher.export('tw', 'map', output_path / 'langmap')
+matcher.export('tw', 'full', output_path / 'twtos-full')
+# matcher.export('tw', 'map', output_path / 'langmap')
 
 # matcher.export('en', 'tw', output_path / 'itos-tw')
 # matcher.export('tw', 'en-opt', output_path / 'twtos-en')
-# matcher.export('tw', 'en-opt', Path(r'C:\TreeOfSaviorTW\release\languageData\twtos-en'))
 # matcher.export('tw', 'dual1', output_path / 'twtos-dual1')
 # matcher.export('tw', 'dual2', output_path / 'twtos-dual2')
-matcher.export('tw', 'dual1', Path(r'C:\TreeOfSaviorTW\release\languageData\twtos-dual1'))
-matcher.export('tw', 'dual2', Path(r'C:\TreeOfSaviorTW\release\languageData\twtos-dual2'))
